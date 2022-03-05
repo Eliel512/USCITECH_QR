668118db5adcdf64614c1b0c8530d5f86945241d
LeonardoRubuz <rubuzleonardo@gmail.com>
2022-03-05 15:29:28 +0100

    Change port

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const normalizePort = val => {
   }
   return false;
 };
-const port = normalizePort(process.env.PORT || '80');
+const port = normalizePort(process.env.PORT || 3000);
 app.set('port', port);
 
 const errorHandler = error => {

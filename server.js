208b65b1d5ae88bcc7676ca97db41b26fc3bcae8
LeonardoRubuz <rubuzleonardo@gmail.com>
2022-03-05 15:20:07 +0100

    Setting PORT to 80 for heroku

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const normalizePort = val => {
   }
   return false;
 };
-const port = normalizePort(process.env.PORT || '3000');
+const port = normalizePort(process.env.PORT || '80');
 app.set('port', port);
 
 const errorHandler = error => {
